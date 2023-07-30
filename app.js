4ec1283476a17be7ac73d3e89bb5bc8ccccfd4b7
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-07-30 23:01:16 +0500

    Updated app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//********* Take input from user and welcome the City **********//
+
+// var city = prompt("ENTER A CITY");
+
+// if (city.toLowerCase() === "karachi") {
+//   alert("'Welcome The City Of Light :'");
+// } else if (city.toLowerCase() === "lahore") {
+//   alert("'Welcome The City Of Traffic :'");
+// } else if (city.toLowerCase() === "kashmir") {
+//   alert("'Welcome The City Of Nature :'");
+// } else {
+//   alert("'NOT WELLCOME :'");
+// }
