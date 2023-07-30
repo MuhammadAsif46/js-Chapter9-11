81dfe25274d702e12bc8f1c20cbf2cec69cb16ed
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-07-30 23:11:38 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -189,3 +189,21 @@
 // }else {
 //     alert("this is an odd number : " + num1);
 // }
+
+
+
+//********* Take input from user and show message what temperature is it **********//
+
+// var temperature = prompt("ENTER TEMPERATURE.");
+
+// if (temperature >= 40){
+//     alert(" “It is too hot outside.” ");
+// }else if (temperature >= 30){
+//     alert(" “The Weather today is Normal.” ");
+// }else if (temperature >= 20){
+//     alert(" “Today’s Weather is cool.” ");
+// }else if (temperature >= 10){
+//     alert(" “OMG! Today’s weather is so Cool.” ");
+// }else {
+//     alert("this number is not allowed");
+// }
